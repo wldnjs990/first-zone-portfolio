4383ae4341b7500b58c858168f3f9118099ccc06
wl990@naver.com <wl990@naver.com>
2023-08-09 16:11:04 +0900

    밀렛 클론코딩 업데이트 8.8 ~ 8.9

--- work/clonecoding/millet/js/common.js
+++ work/clonecoding/millet/js/common.js
@@ -38,4 +38,64 @@ $(document).ready(function(){
     var swiper = new Swiper(".content-1", {
         loop : true,
     });
+    var swiper2 = new Swiper(".item-wrap", {
+        loop : true,
+        slidesPerView: 4,
+    });
+    var swiper3 = new Swiper(".new-swipe", {
+        loop : true,
+        slidesPerView: 4,
+    });
+    var swiper3 = new Swiper(".commend-swipe", {
+        loop : true,
+        slidesPerView: 5,
+    });
+
+    /*헤더 숨기기*/
+    /*--header_scroll---------------------------*/
+    var action_scroll;
+    var now_scroll_top=0;
+    var start_position=5;
+    var nav_bar_height=$('.header').outerHeight();
+
+    //스크롤시에 사용자가 스크롤 했다는 것을 알림
+    $(window).scroll(function(event) {    
+            action_scroll = true;
+        });
+
+    setInterval(function(){ //setInterval : 호출함수(일정시간 간격을 두고 함수 실행)
+        if(action_scroll){
+        hasScrolled();
+        action_scroll=false; //리셋
+        }
+    }, 250);  //250ms(시간단위, 1초 1000분 1)마다
+
+    //has_scrolled()실행하고 action_scroll 상태를 재설정
+
+    //hasScrolled 동작 구현
+    function hasScrolled(){
+        //헤더의 높이보다 더 스크롤이 되었을 경우
+        var st = $(this).scrollTop(); //현재 스크롤의 위치를 저장
+        if(Math.abs(now_scroll_top - st) <= start_position) //현재 스크롤 값 반환
+        return;   //return 실행후 종료, 위치값 호출
+
+            if(st > now_scroll_top && st > nav_bar_height){
+            $('.header').removeClass('header-down').addClass('header-up');
+        } else {
+            if(st + $(window).height()<$(document).height()){
+                $('.header').removeClass('header-up').addClass('header-down');
+            }
+        }
+        now_scroll_top = st;
+    }
+    /*--//header_scroll-------------------------*/
+    
+    /*new-swipe 스크립트*/
+    $('.new-nav > li').click(function(){
+        let idx = $(this).index()
+        $('.new-nav > li').removeClass('on');
+        $(this).addClass('on');
+        $('.new-list').removeClass('on');
+        $('.new-list').eq(idx).addClass('on');
+    })
 });
