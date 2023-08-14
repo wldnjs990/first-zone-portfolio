c323c3deb6b9bdf8452bf6d4fc45f69dda89984e
wl990@naver.com <wl990@naver.com>
2023-08-14 22:34:38 +0900

    23.08.14 오늘과일

--- work/clonecoding/today_fruits/js/script.js
+++ work/clonecoding/today_fruits/js/script.js
@@ -4,8 +4,26 @@ $(document).ready(function(){
     });
     var swiper2 = new Swiper(".special", {
         spaceBetween: 10,
-        slidesPerView : 1.9,
-    });
+        slidesPerView: 1.2,
+        breakpoints: {
 
+            470: {
+              slidesPerView: 2,
+            },
+
+          }
+    });
+    var swiper3 = new Swiper(".new", {
+        spaceBetween: 10,
+        slidesPerView: 2,
+        breakpoints: {
+
+            470: {
+              slidesPerView: 2.9,
+            },
+
+          }
+        
+    });
     
 })
