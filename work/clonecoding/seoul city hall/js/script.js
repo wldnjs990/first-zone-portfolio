d92abb66b36d2eacc6d3f5af3e52f992c4381719
wl990@naver.com <wl990@naver.com>
2023-09-22 00:15:03 +0900

    23.09.22 서울시청

--- work/clonecoding/seoul city hall/js/script.js
+++ work/clonecoding/seoul city hall/js/script.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
 
     const swiper1 = new Swiper('.swipe-1', {
-        auto: true,
+      loop : true,
+      spaceBetween: 40,
+      pagination: {
+        el : ".swiper-pagination-1",
+        type : "fraction",
+      },
+      navigation: {
+        nextEl: ".next-1",
+        prevEl: ".prev-1",
+      },
       });
     const swiper2 = new Swiper(".swipe-2", {
       loop : true,
       slidesPerView: 3,
       spaceBetween: 40,
-      Pagination: {
+      pagination: {
         el : ".swiper-pagination",
         type : "fraction",
+      },
+      navigation: {
+        nextEl: ".next-2",
+        prevEl: ".prev-2",
       },
     });
     
